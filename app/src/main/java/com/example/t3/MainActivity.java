7bc28bcecf8a3b8c8ac9a093148b3ab622f984dc
MelinNathan <nathanmelin49@orange.fr>
2023-12-18 19:40:13 +0100

    fix task1

--- app/src/main/java/com/example/t3/MainActivity.java
+++ app/src/main/java/com/example/t3/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.t3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
@@ -20,69 +16,52 @@ public class MainActivity extends AppCompatActivity {
 
     Button valider;
     String trueUsername = "1";
-
     String truePassword = "1";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=findViewById(R.id.toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        valider = (Button) findViewById(R.id.btn_valider);
+        valider = findViewById(R.id.btn_valider);
         valider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                etUsername = (EditText) findViewById(R.id.et_username);
-                etPassword = (EditText) findViewById(R.id.et_code);
+                etUsername = findViewById(R.id.et_username);
+                etPassword = findViewById(R.id.et_code);
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if ((username.equals(trueUsername)) && (password.equals(truePassword))){
+                if ((username.equals(trueUsername)) && (password.equals(truePassword))) {
                     Toast.makeText(getBaseContext(), "Hello", Toast.LENGTH_LONG).show();
 
-                    Intent intent = new Intent(getBaseContext() , afficherMessage.class);
+                    Intent intent = new Intent(MainActivity.this, afficherMessage.class);
                     intent.putExtra("name", username);
-
                     startActivity(intent);
-                } else{
-                    Toast.makeText(getBaseContext(), "Le mot de passe ou le code secret est incorect", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "Le mot de passe ou le code secret est incorrect", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
     public void onClickValider(View view){
-        etUsername = (EditText) findViewById(R.id.et_username);
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_code);
         String username = etUsername.getText().toString();
-        Intent intent = new Intent(this , afficherMessage.class);
-        intent.putExtra("username", username);
-        //  Toast.makeText(getBaseContext(), "Hello", Toast.LENGTH_LONG).show();
-        startActivity(intent);
-    }
-    @Override
-    public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.main, menu);
-        return (super.onCreateOptionsMenu(menu));
-    }
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
+        String password = etPassword.getText().toString();
 
-        if (item.getItemId() == R.id.itmSaluer) {
-            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
-            return (true);
-        }
-        if (item.getItemId() == R.id.itmEnregistrer) {
-            etUsername = (EditText) findViewById(R.id.et_username);
-            String username = etUsername.getText().toString();
-            Intent intent = new Intent(this, imc.class);
+        if ((username.equals(trueUsername)) && (password.equals(truePassword))) {
+            Toast.makeText(getBaseContext(), "Hello", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(this, afficherMessage.class);
             intent.putExtra("name", username);
             startActivity(intent);
-            return (true);
+        } else {
+            Toast.makeText(getBaseContext(), "Le mot de passe ou le code secret est incorrect", Toast.LENGTH_LONG).show();
         }
-
-        return(super.onOptionsItemSelected(item));
     }
 }
--- app/src/main/java/com/example/t3/afficherMessage.java
+++ app/src/main/java/com/example/t3/afficherMessage.java
@@ -1,46 +1,49 @@
 package com.example.t3;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class afficherMessage extends AppCompatActivity {
-    Bundle bundle;
+
     String user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_afficher_message); // Assurez-vous que le layout correct est utilisé
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-    @Override
-    public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.main, menu);
-        return (super.onCreateOptionsMenu(menu));
-    }
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
 
+        // Récupérer les données passées depuis l'intent
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            user = extras.getString("name");
+            // Faites ce que vous devez faire avec la variable "user"
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.itmSaluer) {
             Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
-            return (true);
+            return true;
         }
         if (item.getItemId() == R.id.itmEnregistrer) {
-//            etUsername = (EditText) findViewById(R.id.et_username);
-//            String username = etUsername.getText().toString();
-            //test
-            Intent intent = new Intent(this, afficherMessage.class);
-            intent.putExtra("name", "test");
-            startActivity(intent);
-            return (true);
+            // Vous pouvez utiliser la variable "user" ici
+            Toast.makeText(this, "User: " + user, Toast.LENGTH_LONG).show();
+            return true;
         }
 
-        return(super.onOptionsItemSelected(item));
+        return super.onOptionsItemSelected(item);
     }
 }
